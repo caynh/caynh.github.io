1755aef0c81ac7f06b28fd833b1cad2d9842699d
caynh <caynhallow@gmail.com>
2025-05-01 12:10:44 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Welcome!</h2>
+        <p>Pursuing Software Engineer with a focus in React and Ruby on Rails. Making code seless and clean one line at a time!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Cayn Hallow</h1>
+        </header>
+    )
+}
