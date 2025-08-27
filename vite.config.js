ff785d38d10fb7e47f3bfa61427f986842b72239
caynh <caynhallow@gmail.com>
2025-08-27 12:33:14 -0400

    Fixes for GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/caynh.github.io/',
+   base: '/',
    server: {
     watch: {
       usePolling: true,
